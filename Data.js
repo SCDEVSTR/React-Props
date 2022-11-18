7103118a47e117644f6903a9ac30657f34ac3a72
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-18 10:30:25 +0300

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    ad: "Engin",
+    yas: "40",
+    picture: "twitter.png"
+  },
+
+  {
+     ad: "Emre",
+    yas: "30",
+    picture: "twitter.png"
+  },
+  {
+    ad: "Su",
+    yas: "4",
+    picture: "twitter.png"
+  },
+];
